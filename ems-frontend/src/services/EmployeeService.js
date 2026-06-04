6cc80c949bcd2f39403d976a16ec8d6327ec1914
vishnuchappidi <vishnu09c@gmail.com>
2026-06-04 13:01:11 +0530

    connect frontend to render backend

--- ems-frontend/src/services/EmployeeService.js
+++ ems-frontend/src/services/EmployeeService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const REST_API_BASE_URL='http://localhost:8080/api/employees';
+const REST_API_BASE_URL = "https://employee-management-system1-1hoq.onrender.com/api/employees";
 export const listEmployees=()=>axios.get(REST_API_BASE_URL);
 
 
